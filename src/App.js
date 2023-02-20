2531703054813ca7a774f5ed2cc036ea028ec386
MuhammadRabius <muhammadrabiussany@gmail.com>
2023-02-20 23:19:20 +0600

    Header and Footer Added and sass error issue solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./component/Footer/Footer";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes></Routes>
+      <Footer />
     </div>
   );
 }
