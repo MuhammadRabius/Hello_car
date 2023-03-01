f1c3a176ca1b12df6f4d5fd81909e3b3a6f22424
MuhammadRabius <muhammadrabiussany@gmail.com>
2023-03-01 23:07:41 +0600

    Search Component Modify

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,16 +1,33 @@
-import React from "react";
-import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { Avatar, Card } from "antd";
+import React, { useEffect, useState } from "react";
+
+import { Avatar, Card, Pagination, Tag } from "antd";
 import Fliter from "./Fliter";
 import SearchBar from "../SearchBar/SearchBar";
 import "./SearchResult.scss";
+import { getLocalDate } from "../../global_stage/action";
+import { DisplayCar } from "../../API/api";
 
 const SearchResult = () => {
   const { Meta } = Card;
+
+  const [carData, setCarData] = useState([]);
+
+  // mycar GET------------------
+
+  useEffect(() => {
+    // const ac = new AbortController();
+
+    (async () => {
+      try {
+        const display = await DisplayCar();
+        setCarData(display.data.data);
+      } catch (err) {
+        console.warn(err.message);
+      }
+    })();
+
+    // return () => ac.abort();
+  }, []);
 
   return (
     <>
@@ -25,29 +42,75 @@ const SearchResult = () => {
               <Fliter />
             </div>
             <div className="card_result">
-              <Card
-                style={{
-                  width: 280,
-                }}
-                cover={
-                  <img
-                    alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                  />
-                }
-                actions={[
-                  <SettingOutlined key="setting" />,
-                  <EditOutlined key="edit" />,
-                  <EllipsisOutlined key="ellipsis" />,
-                ]}
-              >
-                <Meta
-                  avatar={<Avatar src="https://joesch.moe/api/v1/random" />}
-                  title="Card title"
-                  description="This is the description"
-                />
-              </Card>
+              {carData.map((item, i) => {
+                return (
+                  <>
+                    {item.offerPrice ? (
+                      <Card
+                        key={i}
+                        style={{
+                          width: 280,
+                        }}
+                        cover={
+                          <img
+                            alt="carimage"
+                            src={
+                              item.image === ""
+                                ? `https://i.ibb.co/wyDzzLm/Car-01-1.jpg`
+                                : item.image
+                            }
+                          />
+                        }
+                      >
+                        <Meta title={`Card Brand  : ${item.brandName}`} />
+                        <p className="text_group">
+                          Car Model : ${item.carModel}
+                        </p>
+                        <p className="text_group">
+                          Release Date : {getLocalDate(item.releaseDate)}
+                        </p>
+                        <p className="text_group">
+                          Buying Price :{" "}
+                          <Tag color="#2db7f5">{item.buyingPrice}</Tag>
+                        </p>
+                        <p className="text_group">
+                          Selling Price :
+                          {item.offerPrice ? (
+                            <Tag color="red">
+                              <del>{item.sellPrice}</del>
+                            </Tag>
+                          ) : (
+                            <Tag color="#87d068">{item.sellPrice}</Tag>
+                          )}
+                        </p>
+
+                        <p className="text_group">
+                          Offer Price :
+                          {item.offerPrice ? (
+                            <Tag color="#87d068">{item.offerPrice}</Tag>
+                          ) : (
+                            "Not Set Yet!!"
+                          )}
+                        </p>
+                        <p className="text_group">
+                          Description :
+                          {item.description
+                            ? `${item.description}`
+                            : "No Description"}
+                        </p>
+                      </Card>
+                    ) : null}
+                  </>
+                );
+              })}
             </div>
+            {/* Add Pagination */}
+            <Pagination
+              style={{ marginBottom: "20px", textAlign: "end" }}
+              defaultCurrent={1}
+              pageSize={6}
+              total={carData.length}
+            />
           </div>
         </div>
       </div>
